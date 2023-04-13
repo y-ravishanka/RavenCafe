beaaa4d660aca0b334bc4e907345eb6a07dc02c8
y-ravishanka <ravishanka.yasas@gmail.com>
2023-04-13 19:25:39 +0530

    Add project files.

---
+++ server/RavenCafe_API/RavenCafe_API/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RavenCafe_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
